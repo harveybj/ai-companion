4aaefb10ca97843e6ca4fcd938819312db7b0cad
harvey bj <harveybj@yahoo.com>
2025-03-04 12:09:37 -0800

    added prisma db and categories

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,13 @@
 import { SearchInput } from '@/components/search-input';
+import prisma from '@/lib/prismadb';
+import { Categories } from '@/components/categories';
 
-const RootPage = () => {
+const RootPage = async () => {
+  const categories = await prisma.category.findMany();
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
   );
 };
